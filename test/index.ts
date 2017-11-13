6dae47d0782f6edb700573ea24cfe3ca6f47bd4a
Federico Knüssel <fknussel@gmail.com>
2017-11-13 17:12:52 -0300

    Remove old tests

--- test/index.ts
+++ test/index.ts
@@ -1,44 +1,10 @@
-// import * as mocha from 'mocha';
-// import * as chai from 'chai';
-// import chaiHttp = require('chai-http');
-// import App from '../src/App';
+import * as mocha from 'mocha';
+import * as chai from 'chai';
 
-// chai.use(chaiHttp);
+const expect = chai.expect;
 
-// const expect = chai.expect;
-// const mockCollection = {
-
-// };
-// const app = new App();
-
-// describe('baseRoute', () => {
-
-//   it('should return a valid json response', () => {
-//     return chai
-//       .request(app)
-//       .get('/')
-//       .then(response => {
-//         expect(response.type).to.eql('application/json');
-//       });
-//   });
-
-//   it('should return a meta property', () => {
-//     return chai
-//       .request(app)
-//       .get('/')
-//       .then(response => {
-//         expect(response.body.meta.message).to.eql('Success');
-//         expect(response.body.meta.status).to.eql(200);
-//       });
-//   });
-
-//   it('should return a data property', () => {
-//     return chai
-//       .request(app)
-//       .get('/')
-//       .then(response => {
-//         expect(response.body.data).to.eql(null);
-//       });
-//   });
-
-// });
+describe('example', () => {
+  it('should work', () => {
+    expect(true).to.exist;
+  });
+});
