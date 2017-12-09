6f3d3859b21976cb5a0b6b66b639423b78d64f7a
Federico Knüssel <fknussel@gmail.com>
2017-12-09 09:38:51 -0300

    Make this repository public

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # places-api
 
+## Running locally
+
+```
+MONGODB_PASSWORD=X yarn start
+```
+
+## Credits
+
 Based off http://mherman.org/blog/2016/11/05/developing-a-restful-api-with-node-and-typescript/
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
 // const { MONGODB_USERNAME, MONGODB_PASSWORD } = process.env;
 const MONGODB_USERNAME = 'placelog';
-const MONGODB_PASSWORD = 'golecalp';
+const MONGODB_PASSWORD = process.env.MONGODB_PASSWORD;
 
 export const MONGODB_URI = `mongodb://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@ds159235.mlab.com:59235/placelog`;
 export const PLACES_COLLECTION = 'places';
